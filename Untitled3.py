08cfb177094f8777ed6128a8923c3d82e903e036
Ghanshyam Kumar <69349866+Ghanshyam-Coder@users.noreply.github.com>
2021-02-06 20:10:07 +0530

    Add files via upload

---
+++ Untitled3.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def fibonacci(num):
+    num1=0
+    num2=1
+    series=0
+    for i in  range(num):
+        print(series, end='');
+        num1=num2;
+        num2=series;
+        series=num1+num2;
+        
+#running function after taking user input
+num=int(input('Enter how many numbers needes in Fibonacci series-'))
+fibonacci(num)
+
+
+# In[ ]:
+
+
+
+
